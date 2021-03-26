bdc28d1cd3427d333c1cf589633ed43540e41ca5
eamtcPROG <prog.eamtc@gmail.com>
2021-03-26 22:29:24 +0200

    fix 4

--- api/server.js
+++ api/server.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 
-
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@my-first-cluster.gdyz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
     { useNewUrlParser: true, useUnifiedTopology: true }
@@ -16,8 +15,8 @@ mongoose.connect(
 
 
 const server = express();
-//const whitelist = ['http://localhost:3000','https://notes-app-exercise.vercel.app'];
-/* const corsOptions = {
+const whitelist = ['http://localhost:3000','https://notes-app-exercise.vercel.app'];
+const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
@@ -26,9 +25,9 @@ const server = express();
     }
   },
   credentials: true,
-}; */
+};
 
-//server.use(cors(corsOptions));
+server.use(cors(corsOptions));
 server.use(cors());
 server.use(bodyParser.json());
 //server.use(express.json());
